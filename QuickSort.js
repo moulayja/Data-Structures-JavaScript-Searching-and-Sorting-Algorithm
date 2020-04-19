cc20602bc106cf2a94b703234ad673df86ce62b7
Jamal <moulayjam@gmail.com>
2020-04-19 12:57:47 -0400

    QuickSort Algorithm Solution

--- QuickSort.js
+++ QuickSort.js
@@ -1,4 +1,31 @@
 /*Quick sort is a comparison sort, meaning that it can sort items of
 any type for which a "less-than" relation (formally, a total order) is defined */
 
+function quick_Sort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
 
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+	}
+}
+
+var myArray = [3, 0, 2, 5, -1, 4, 1 ];
+
+console.log("Original array: " + myArray);
+var sortedArray = quick_Sort(myArray);
+console.log("Sorted array: " + sortedArray);
