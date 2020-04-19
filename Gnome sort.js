6193e5c856a8fb4dca9272a8a08314c925d36286
Jamal <moulayjam@gmail.com>
2020-04-19 13:20:57 -0400

    Gnome Sort Solution

--- Gnome sort.js
+++ Gnome sort.js
@@ -5,3 +5,27 @@
 // The algorithm always finds the first place where two adjacent elements are in the wrong order,
 // and swaps them. It takes advantage of the fact that performing a swap can introduce a new out-of-order adjacent pair only 
 // next to the two swapped elements.
+
+function gnomeSort(arr) 
+{
+    function moveBack(i) 
+  {
+        for( ; i > 0 && arr[i-1] > arr[i]; i--)
+        {
+            var t = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = t;
+        }
+    }
+    for (var i = 1; i < arr.length; i++) 
+    {
+        if (arr[i-1] > arr[i]) moveBack(i);
+    }
+    return arr;
+}
+
+var arra = [3, 0, 2, 5, -1, 4, 1]; 
+console.log("Original Array Elements"); 
+console.log(arra); 
+console.log("Sorted Array Elements"); 
+console.log(gnomeSort(arra));
