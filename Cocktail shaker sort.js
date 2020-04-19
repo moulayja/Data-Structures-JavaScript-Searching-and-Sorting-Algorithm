3c226baf05859f3fc88aa232790312fd90f86c48
Jamal <moulayjam@gmail.com>
2020-04-19 13:14:27 -0400

    Cocktail Shaker Def

---
+++ Cocktail shaker sort.js
@@ -0,0 +1,9 @@
+// Cocktail shaker sort (also known as bidirectional bubble sort, cocktail sort, shaker sort,
+// ripple sort, shuffle sort, or shuttle sort ) is a variation of bubble sort that is both a stable sorting algorithm
+// and a comparison sort. The algorithm differs from a bubble sort in that it sorts in both directions on each pass through 
+// the list. This sorting algorithm is only marginally more difficult to implement than a bubble sort,
+// and solves the problem of turtles in bubble sorts. It provides only marginal performance improvements,
+// and does not improve asymptotic performance; like the bubble sort, it is not of practical interest,
+// though it finds some use in education.
+
+
