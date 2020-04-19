ed45b79f3c35db93402e810287473d6586dd71e6
Jamal <moulayjam@gmail.com>
2020-04-19 13:32:22 -0400

    BogoSort Def

---
+++ Bogosort.js
@@ -0,0 +1,5 @@
+// In computer science, bogosort is a particularly ineffective sorting algorithm based on the generate and test paradigm. 
+// The algorithm successively generates permutations of its input until it finds one that is sorted.
+// It is not useful for sorting, but may be used for educational purposes, to contrast it with other more realistic algorithms.
+
+
