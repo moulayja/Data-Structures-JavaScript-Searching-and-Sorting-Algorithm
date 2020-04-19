5285bb2a562db537d82ef85e79971631e7aa8b7a
Jamal <moulayjam@gmail.com>
2020-04-19 12:55:57 -0400

    Merge Sort Definition

---
+++ Merge sort.js
@@ -0,0 +1,4 @@
+// According to Wikipedia "Merge sort (also commonly spelled mergesort) is an O (n log n) 
+// comparison-based sorting algorithm. Most implementations produce a stable sort,
+// which means that the implementation preserves the input order of equal elements
+//  in the sorted output."
